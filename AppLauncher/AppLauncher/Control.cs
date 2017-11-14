2eb794a52299e294c133c2b7b57bc2f3a6de7ab0
adamp <adamp@ADAM-PC>
2017-11-14 07:16:56 +0100

    - Copy, Delete, Info

---
+++ AppLauncher/AppLauncher/Control.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace AppLauncher
+{
+    class Control
+    {
+
+        public string FilePath;
+        public string Lines;
+
+        public override string ToString()
+        {
+            return FilePath;
+        }
+        public string[] GetData(string pathToFile)
+        {
+            string[] Lines = File.ReadAllLines(pathToFile);
+            return Lines;
+        }
+        public string GetNotes(string pathToNotes)
+        {
+            string Notes = File.ReadAllText(pathToNotes);
+            return Notes;
+        }
+        public void CheckExists(string pathToFile)
+        {
+            if (!File.Exists(pathToFile + ".csv"))
+            {
+                File.CreateText(pathToFile + ".csv").Close();
+            }
+        }
+        public void CopyFiles(string SourcePath, string DestinationPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+            SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+            }
+
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+                SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+            }
+        }
+        public void deleteFile(string path)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+
+        
+    }
+}
--- AppLauncher/AppLauncher/MainWindow.xaml.cs
+++ AppLauncher/AppLauncher/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace AppLauncher
 {
@@ -20,9 +25,118 @@ namespace AppLauncher
     /// </summary>//
     public partial class MainWindow : Window
     {
+        ObservableCollection<Control> dirList = new ObservableCollection<Control>();
+        ObservableCollection<Control> infoDirList = new ObservableCollection<Control>();
+        ObservableCollection<Control> RootDirList = new ObservableCollection<Control>();
         public MainWindow()
         {
             InitializeComponent();
+            getRootDirs();
+            ProjectsView.ItemsSource = dirList;
+
+        }
+        public void getRootDirs()
+        {
+            Control dbFile = new Control();
+            string[] resLines = dbFile.GetData("data.csv");
+            foreach(var line in resLines)
+            {
+                searchFiles(line);
+            }
+        }
+        public void searchFiles(string searchedDir)
+        {
+            string[] dirs = Directory.GetFiles(@"" + searchedDir + "", "*.sln", SearchOption.AllDirectories);
+            showAllFiles(searchedDir, dirs);
+        }
+        public void showAllFiles(string searchedDir, string[] dirs)
+        {
+            
+            foreach (string dir in dirs)
+            {
+                Control dataFileRes = new Control();
+                Control infoFileRes = new Control();
+                Control rootFileRes = new Control();
+
+                string newDir = System.IO.Path.ChangeExtension(dir, null);
+                string fileName = System.IO.Path.GetFileNameWithoutExtension(dir);
+
+                string dirToFile = (newDir + @"\" + @"bin\Debug\" + (fileName) + ".exe" + "");
+                string dirToInfoFile = (newDir + @"\" + @"bin\Debug\" + "appInfo" + ".csv" + "");
+                string dirToRootFolder = (newDir.Replace(@"\" + fileName, ""));
+                string finalDirToRootFolder = dirToRootFolder + @"\" + fileName;
+
+                //Kontrola zda existuje soubor pro poznámky
+                rootFileRes.CheckExists(newDir);
+
+                dataFileRes.FilePath = dirToFile;
+                infoFileRes.FilePath = (newDir + ".csv");
+                rootFileRes.FilePath = finalDirToRootFolder;
+
+                dirList.Add(dataFileRes);
+                infoDirList.Add(infoFileRes);
+                RootDirList.Add(rootFileRes);
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedItem = ProjectsView.SelectedIndex;
+            string processToRun = dirList[selectedItem].FilePath;
+            string pathToStart = (@"" + processToRun + "");
+
+            Process.Start(pathToStart);
+        }
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedItem = ProjectsView.SelectedIndex;
+            string path = infoDirList[selectedItem].FilePath;
+            Control infoNotes = new Control();
+            string infoData = infoNotes.GetNotes(path);
+            infoBox.Text = infoData;
+            string pathToNotes = e.AddedItems[0].ToString();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int selectedItem = ProjectsView.SelectedIndex;
+            string path = infoDirList[selectedItem].FilePath;
+            string content = infoBox.Text;
+            File.WriteAllText(path, content);
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int selectedItem = ProjectsView.SelectedIndex;
+            string path = RootDirList[selectedItem].FilePath;
+            string SourcePath = path;
+            string DestinationPath = dir.Text;
+
+            //Kopírování do nové složky
+            Control dataHandler = new Control();
+            dataHandler.CopyFiles(SourcePath, DestinationPath);          
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                dir.Text = dialog.FileName;
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            int selectedItem = ProjectsView.SelectedIndex;
+            string path = RootDirList[selectedItem].FilePath;
+
+            // Mazání Adresářů
+            Control dataHandler = new Control();
+            dataHandler.deleteFile(path);
         }
     }
 }
